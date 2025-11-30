eb4a9376f34227ceadb6935d32a5132ed45d326b
jayshreeparde21 <your_github_email@gmail.com>
2025-11-30 23:44:30 +0530

    backend commit

---
+++ ems-backend/src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package net.javaguides.ems.repository;
+
+import jakarta.transaction.Transactional;
+import net.javaguides.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
